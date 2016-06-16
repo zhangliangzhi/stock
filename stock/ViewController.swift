b7b96c83d37388d90d155d5c5ce7e0bb03a96a1a
xigk <3717591@qq.com>
2016-06-16 19:32:45 +0800

    init stock by swift

---
+++ stock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stock
+//
+//  Created by xigk on 16/6/16.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
